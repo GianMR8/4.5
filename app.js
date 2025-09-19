53fb38f952cc8293087bd6847756acbe5dfe2d41
GianMR8 <gm807077@gmail.com>
2025-09-19 11:36:31 -0500

    commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// Concatenating strings
+let fullName = firstName + " " + lastName;
+console.log(fullName); //Output: Stan Lee
+    
+// Concatenating strings and a number
+let userInfo = "Name: " + fullName + ", Age: " + age;
+console.log(userInfo); // Output: Name: Stan Lee, Age: 17
+
+let month = November;
+let text = "It's Thanksgiving";
+
+if (month > December) {
+    text = "It's Christmas!";
+}
+
+switch (new Date().getMonth()) {
+  case 1:
+    date = "January";
+    break;
+  case 2:
+    date = "February";
+    break;
+  case 3:
+    date = "March";
+    break;
+  case 4:
+    date = "April";
+    break;
+  case 5:
+    date = "May";
+    break;
+  case 6:
+    date = "June";
+    break;
+  case 7:
+    date = "July";
+    break;
+  case 8:
+    date = "August";
+    break;
+  case 9:
+    date = "September";
+    break;
+  case 10:
+    date = "October";
+    break;
+  case 11:
+    date = "November";
+    break;
+  case 12:
+    date = "December";
+    break;
+}
+
+let firstName = "Stan";
+let lastName = "Lee";
+let age = 17;
+
+let x = 500;
+x.toString();
+(500).toString();
+(250 + 250).toString();
+
+// {} is an object
